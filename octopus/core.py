d9106df80d7dfee48417e29e606b15b99ebf241a
Bernardo Heynemann <heynemann@gmail.com>
2013-11-15 16:27:13 -0200

    Started the queue processing and it works

--- octopus/core.py
+++ octopus/core.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+from threading import Thread
 
+import requests
 from six.moves import queue
 
 
 class Octopus(object):
-    def __init__(self, concurrency=10):
+    def __init__(self, concurrency=10, auto_start=False):
         self.concurrency = concurrency
+        self.auto_start = auto_start
 
         self.url_queue = queue.Queue()
 
-    def enqueue(self, url):
-        self.url_queue.put_nowait(url)
+    def enqueue(self, url, handler):
+        self.url_queue.put_nowait((url, handler))
 
     @property
     def queue_size(self):
         return self.url_queue.qsize()
+
+    @property
+    def is_empty(self):
+        return self.url_queue.empty()
+
+    def start(self):
+        for i in range(self.concurrency):
+            t = Thread(target=self.do_work)
+            t.daemon = True
+            t.start()
+
+    def do_work(self):
+        while True:
+            url, handler = self.url_queue.get()
+
+            response = requests.get(url)
+            handler(response)
+
+            self.url_queue.task_done()
--- tests/test_octopus.py
+++ tests/test_octopus.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+
 from preggy import expect
 
 from octopus import Octopus
@@ -11,14 +13,38 @@ class TestOctopus(TestCase):
     def test_can_create_octopus(self):
         otto = Octopus(concurrency=20)
         expect(otto.concurrency).to_equal(20)
+        expect(otto.auto_start).to_be_false()
 
     def test_has_default_concurrency(self):
         otto = Octopus()
         expect(otto.concurrency).to_equal(10)
 
+    def test_queue_is_empty(self):
+        otto = Octopus()
+        expect(otto.is_empty).to_be_true()
+
     def test_can_enqueue_url(self):
         otto = Octopus()
 
-        otto.enqueue('http://www.google.com')
+        otto.enqueue('http://www.google.com', None)
 
         expect(otto.queue_size).to_equal(1)
+
+    def test_can_get_after_started(self):
+        otto = Octopus(concurrency=1)
+
+        self.response = None
+
+        def handle_url_response(response):
+            self.response = response
+
+        otto.enqueue('http://www.google.com', handle_url_response)
+        otto.start()
+
+        tried = 0
+        while not self.response and tried < 30:
+            tried += 1
+            time.sleep(0.1)
+
+        expect(self.response).not_to_be_null()
+        expect(self.response.status_code).to_equal(200)
