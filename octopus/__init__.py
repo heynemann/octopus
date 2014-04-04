c7bc93e60530368b137a4e9df26b5b34dacecedf
Bernardo Heynemann <heynemann@gmail.com>
2014-04-04 11:20:30 -0300

    Release 0.6.4

--- octopus/__init__.py
+++ octopus/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 from octopus.core import Octopus, TimeoutError, ResponseError  # NOQA
 from octopus.tornado_core import TornadoOctopus  # NOQA
